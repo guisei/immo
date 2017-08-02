62d97dc383e38b0b99aae16b6ee4c6336b8bd28d
guisei <seiler.guillaume@gmail.com>
2017-08-02 14:24:19 +0200

    modifs et ajout commentaires

--- immo_crawler/spiders/leboncoin_buy.py
+++ immo_crawler/spiders/leboncoin_buy.py
@@ -8,15 +8,13 @@ class ListingsSpider(scrapy.Spider):
     name = 'leboncoin_buy'
     allowed_domains = ['leboncoin.fr']
     start_urls = ['https://www.leboncoin.fr/ventes_immobilieres/offres/ile_de_france/paris/?th=1&ret=1&ret=2']
-    custom_settings = {'FEED_EXPORT_FIELDS': ["listingId", "url", "title", "date", "datetime", "price", "location",
-                                              "propertyType", "rooms", "superficy", "description"]}
     urls = []
 
     def parse(self, response):
-        stop = False
         urls = response.xpath("//*[@id='listingAds']/section/section/ul/li/a/@href").extract()
         for url in urls:
             url = "https:" + str(url)
+            #if this listing already exists in the database (list self.urls) then we don't open it and remove this url from the list. After the script is finished, we update status = 0 for all urls that was not delete (see pipelines.py)
             if url in self.urls:
                 self.urls.remove(url)
                 continue
@@ -33,8 +31,10 @@ class ListingsSpider(scrapy.Spider):
         items['title'] = response.xpath("//h1[@itemprop='name']/text()").extract_first().strip()
         items['date'] = response.xpath("//p[@itemprop='availabilityStarts']/@content").extract_first()
         price = response.xpath("//h2[span='Prix']/span[@class='value']/text()").extract_first()
+        #sometimes, the price doest not exist on the listing and if the price doesn't exist then variable is None, but None not has method .strip() and the script has error in this case
         if price:
             items['price'] = price.strip()
+        #this structure allows to take into account the cities that have two words
         items['zip_code'] = response.xpath("//h2/span[@itemprop='address']/text()").extract_first().strip().split()[-1]
         items['city'] = \
             ' '.join(response.xpath("//h2/span[@itemprop='address']/text()").extract_first().strip().split()[:-1])
